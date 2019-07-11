9bd945211cb933f891cf06b2ba3ae85513c8ea69
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-07-11 18:58:51 +0900

    # ZigzagListActivity 수정

--- Zigzag/app/src/main/java/com/example/zigzag/view/list/ZigzagListActivity.kt
+++ Zigzag/app/src/main/java/com/example/zigzag/view/list/ZigzagListActivity.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.zigzag.R
+import com.example.zigzag.common.BaseInfo
 import com.example.zigzag.databinding.ActivityZiagzagListBinding
 import com.example.zigzag.model.list.Shop
 import com.example.zigzag.model.list.ShopInfo
@@ -21,11 +22,11 @@ import java.util.*
 import kotlin.Comparator
 import kotlin.collections.ArrayList
 
-class ZigzagListActivity : AppCompatActivity(),ZigzagListNavigator {
+class ZigzagListActivity : AppCompatActivity(), ZigzagListNavigator {
 
     lateinit var binding: ActivityZiagzagListBinding
     lateinit var viewModel: ZigzagListViewModel
-    lateinit var adapter:ZigzagListAdapter
+    lateinit var adapter: ZigzagListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +41,8 @@ class ZigzagListActivity : AppCompatActivity(),ZigzagListNavigator {
 
         initRecyclerView()
 
+        initObserve()
+
     }
 
     fun initRecyclerView() {
@@ -49,10 +52,73 @@ class ZigzagListActivity : AppCompatActivity(),ZigzagListNavigator {
 
         binding.rcvZigzagList.adapter = adapter
 
-        val itemDecorator = DividerItemDecoration(this,LinearLayoutManager(this).orientation)
+        val itemDecorator = DividerItemDecoration(this, LinearLayoutManager(this).orientation)
         binding.rcvZigzagList.addItemDecoration(itemDecorator)
     }
 
+
+    fun initObserve() {
+        viewModel.observeAgeFilter.observe(this, androidx.lifecycle.Observer {
+
+            it.run {
+                if (viewModel.observeAgeFilter.value == 0 && viewModel.observeStyleFilter.value == 0) {
+                    reduceMapView()
+                } else {
+                    extendMapView()
+
+                    setFilterText()
+                }
+
+            }
+
+        })
+    }
+
+    fun setFilterText() {
+        var filterViewText = ""
+
+        if (viewModel.ageFilter != 0) {
+            //연령대 필터 텍스트
+            for (i in 0..6) {
+                if (viewModel.ageFilter and (1 shl i) == 1 shl i) {
+                    filterViewText += BaseInfo.ageType.get(i)
+                    if (i != 6)
+                        filterViewText += ","
+                }
+            }
+
+            if (filterViewText[(filterViewText.length - 1)] == ',')
+                filterViewText = filterViewText.substring(0, filterViewText.length - 1)
+        }
+
+        if (viewModel.styleFilter != 0) {
+
+            if (viewModel.ageFilter != 0)
+                filterViewText += '/'
+
+            //스타일 필터 텍스트
+            for (i in 0..13) {
+                if ((viewModel.styleFilter and (1 shl i)) == 1 shl i) {
+                    filterViewText += BaseInfo.styleTypeNumToString.get(i)
+                    if (i != 13)
+                        filterViewText += ","
+                }
+            }
+
+            if (filterViewText.get(filterViewText.length - 1) == ',')
+                filterViewText = filterViewText.substring(0, filterViewText.length - 1)
+        }
+
+        viewModel.filterViewText.set(filterViewText)
+    }
+
+    fun extendMapView() {
+        binding.glEndOfFilterText.setGuidelinePercent(0.12.toFloat())
+    }
+
+    fun reduceMapView() {
+        binding.glEndOfFilterText.setGuidelinePercent(0.07.toFloat())
+    }
 
     fun setDataSet() {
         adapter = ZigzagListAdapter(viewModel.getShopListFromJson())
@@ -60,11 +126,33 @@ class ZigzagListActivity : AppCompatActivity(),ZigzagListNavigator {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100 && resultCode == 101) {
+            viewModel.ageFilter = data?.getStringExtra("ageFilter")?.toInt() ?: 0
+            viewModel.styleFilter = data?.getStringExtra("styleFilter")?.toInt() ?: 0
+
+            viewModel.observeAgeFilter.value = viewModel.ageFilter
+            viewModel.observeStyleFilter.value = viewModel.styleFilter
+
+            if (viewModel.ageFilter > 0 && viewModel.styleFilter > 0) {
+                //두 개 필터 모두 선택
+                adapter.setItem(viewModel.getListByAgeAndStyle())
+            } else if (viewModel.ageFilter > 0) {
+                adapter.setItem(viewModel.getListByAgeFilter())
+            } else if (viewModel.styleFilter > 0) {
+                adapter.setItem(viewModel.getListByStyleFilter())
+            } else {
+                //필터 없음
+                adapter.setItem(viewModel.shopInfo.get()?.list ?: ArrayList())
+            }
+
+
+        }
     }
 
     override fun onFilterClick() {
-        val intent = Intent(this,FilterActivity::class.java)
-        startActivityForResult(intent,100)
+        val intent = Intent(this, FilterActivity::class.java)
+        startActivityForResult(intent, 100)
     }
 
 }
