bcc8da0ec1deb841fcfdd06fe49f772fa8fb400b
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-07-11 18:58:25 +0900

    # FilterViewModel 작성

--- Zigzag/app/src/main/java/com/example/zigzag/view/filter/FilterViewModel.kt
+++ Zigzag/app/src/main/java/com/example/zigzag/view/filter/FilterViewModel.kt
@@ -1,10 +1,34 @@
 package com.example.zigzag.view.filter
 
-
 import androidx.lifecycle.ViewModel
 
 class FilterViewModel : ViewModel() {
 
+    var ageFilter = 0
+    var styleFilter = 0
 
+    lateinit var navigator: FilterNavigator
+
+    fun onAgeFilterClick(position: Int) {
+
+        if( (ageFilter and (1 shl position)) != (1 shl position)) {
+            ageFilter = ageFilter or (1 shl position)
+        } else {
+            ageFilter = ageFilter and (1 shl position).inv()
+        }
+    }
+
+    fun onStyleFilterClick(position: Int) {
+
+        if( (styleFilter and (1 shl position)) != (1 shl position)) {
+            styleFilter = styleFilter or (1 shl position)
+        } else {
+            styleFilter = styleFilter and (1 shl position).inv()
+        }
+    }
+
+    fun onFilterFinishClick() {
+        navigator.onFilterFinishClick()
+    }
 
 }
