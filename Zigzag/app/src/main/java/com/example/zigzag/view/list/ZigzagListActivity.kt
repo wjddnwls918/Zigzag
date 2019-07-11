8f64f7b2fc767c1ea646cde4979eebd9c6648cf8
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-07-11 19:01:43 +0900

    # ZigzagListActivity 수정

--- Zigzag/app/src/main/java/com/example/zigzag/view/list/ZigzagListActivity.kt
+++ Zigzag/app/src/main/java/com/example/zigzag/view/list/ZigzagListActivity.kt
@@ -3,34 +3,25 @@ package com.example.zigzag.view.list
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.widget.LinearLayout.HORIZONTAL
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.zigzag.R
 import com.example.zigzag.common.BaseInfo
-import com.example.zigzag.databinding.ActivityZiagzagListBinding
-import com.example.zigzag.model.list.Shop
-import com.example.zigzag.model.list.ShopInfo
+import com.example.zigzag.databinding.ActivityZigzagListBinding
 import com.example.zigzag.view.filter.FilterActivity
-import java.util.*
-import kotlin.Comparator
 import kotlin.collections.ArrayList
 
 class ZigzagListActivity : AppCompatActivity(), ZigzagListNavigator {
 
-    lateinit var binding: ActivityZiagzagListBinding
+    lateinit var binding: ActivityZigzagListBinding
     lateinit var viewModel: ZigzagListViewModel
     lateinit var adapter: ZigzagListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_ziagzag_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_zigzag_list)
 
         viewModel = ViewModelProviders.of(this).get(ZigzagListViewModel::class.java)
         binding.viewmodel = viewModel
